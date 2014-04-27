88061c2e0b3d112dab47905d60e189b7de26ef33
richygit <richard@getup.org.au>
2014-04-28 00:36:02 +1000

    match is accessible

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,7 +3,7 @@ class Match < ActiveRecord::Base
   belongs_to :second_team, foreign_key: :team2_id, class_name: 'Team'
   belongs_to :venue
 
-  attr_accessible :id, :kickoff, :team1, :team2, :venue_id, :stage
+  attr_accessible :id, :kickoff, :team1_id, :team2_id, :venue_id, :stage
 
   scope :with_teams_and_venues, -> { includes(:first_team).includes(:second_team).includes(:venue) }
 
