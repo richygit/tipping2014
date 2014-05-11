e8d9b1f79d9c9d91e69beaab2feaf995fe6669bd
richygit <richard@getup.org.au>
2014-05-11 16:23:22 +1000

    prevent ajax request when team is already selected

---
+++ app/assets/javascripts/tips.js
@@ -0,0 +1,32 @@
+function configure_tips() {
+  $('.tip-container form').each(function(index, form) {
+    console.log('attaching ajax listener.');
+    $(form).on('ajax:before', function() {
+      $(form).find(".teams").prepend("<div class='loading'></div>");
+      console.log("loading");
+    });
+    $(form).on('ajax:success', function(data, status, xhr) {
+      console.log("success");
+    });
+    $(form).on('ajax:failure', function() {
+      console.log("failure");
+      //reset to old value and animate
+    });
+    $(form).on('ajax:complete', function() {
+      $(form).find('.loading').remove();
+      $(form).find(".btn-primary").removeClass("active");
+      $(form).find("input[type=radio]:checked").parents(".btn-primary").addClass("active");
+    });
+  });
+
+  $('.tip-container .btn-primary').each(function(index, input) {
+    $(input).click(function() {
+      var selected = $(event.target).parents("form").find("input[type=radio]:checked").val();
+      var intended = $(event.target).closest(".btn-primary").find("input[type=radio]").val();
+      if(selected != intended) {
+        $(event.target).closest(".btn-primary").find("input[type=radio]").first().prop("checked", true);
+        $(event.target).parents('form').trigger("submit.rails");
+      }
+    });
+  });
+}
