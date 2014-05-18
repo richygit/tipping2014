6fd8e7b782ac0f58af7ecf47ea94aae1ca5411a4
richygit <richard@getup.org.au>
2014-05-19 00:34:31 +1000

    updated default mail host in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Tipping2014::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  config.action_mailer.default_url_options = { host: 'http://tipping2014.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
