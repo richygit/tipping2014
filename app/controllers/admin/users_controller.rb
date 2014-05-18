6193ee78edc5c86613b3c1f6188cbe88516f3620
richygit <richard@getup.org.au>
2014-05-18 22:35:12 +1000

    fixed admin users paths in controllers

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -17,10 +17,10 @@ class Admin::UsersController < ApplicationController
     user.password = Devise.friendly_token.first(8)
     if user.save
       flash[:notice] = "User created"
-      redirect_to 'index'
+      redirect_to admin_users_path
     else
       flash[:alert] = "Error creating user"
-      render 'new'
+      render new_admin_user_path
     end
   end
 
@@ -32,7 +32,7 @@ class Admin::UsersController < ApplicationController
     @user.name = params[:user][:name]
     @user.from = params[:user][:from]
     @user.save!
-    redirect_to 'index'
+    redirect_to admin_users_path
   end
 
   def destroy
