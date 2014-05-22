f7cb9ff5b4baf47bc0396edca88b5779f97920be
richygit <richard@getup.org.au>
2014-05-23 00:18:28 +1000

    added tip helper spec

--- spec/helpers/tips_helper_spec.rb
+++ spec/helpers/tips_helper_spec.rb
@@ -1,15 +1,38 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the TipsHelper. For example:
-#
-# describe TipsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 describe TipsHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#can_tip?" do
+    it "should return false if both teams have not been decided" do
+      @match = create(:match, first_team: nil, second_team: nil, kickoff: 1.week.since)
+      @result = create(:result, match: @match, first_team_score: nil, second_team_score: nil, winner_team_id: nil)
+      helper.can_tip?(@match).should == false
+
+      first_team = create(:team)
+      @match.first_team = first_team
+      helper.can_tip?(@match).should == false
+
+      second_team = create(:another_team)
+      @match.second_team = second_team
+      @match.first_team = nil
+      helper.can_tip?(@match).should == false
+    end
+
+    it "should return false when a score or winner has been determined" do
+      @match = create(:match, kickoff: 1.week.since)
+      @result = create(:result, match: @match, first_team_score: 1, second_team_score: 2, winner_team_id: @match.first_team.id)
+      helper.can_tip?(@match).should == false
+    end
+
+    it "should return false when the match has already started" do
+      @match = create(:match, kickoff: 1.week.ago)
+      @result = create(:result, match: @match)
+      helper.can_tip?(@match).should == false
+    end
+
+    it "should return true if teams are present, no result logged and match has not started" do
+      @match = create(:match, kickoff: 1.week.since)
+      @result = create(:result, match: @match, first_team_score: nil, second_team_score: nil, winner_team_id: nil)
+      helper.can_tip?(@match).should == true
+    end
+  end
 end
