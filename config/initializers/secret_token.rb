b78ec81f943e1ac58f329c718018ac3dbe63f811
richygit <richard@getup.org.au>
2014-07-23 22:03:55 +1000

    removed secret token from repo and added to heroku env

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Tipping2014::Application.config.secret_token = '215db1dcef159edfc805584aa681336ba1eb71a6db46cb5f3d0f4c5433ca4866f0c4396ad36fa030988c080af9091a87233050048d9373cf66f4c53b6df3dca5'
+Tipping2014::Application.config.secret_token = ENV['SECRET_TOKEN']
